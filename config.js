7102ddb59e5bb4373bb5867bb57bcf564e38cf9b
Flaminghurricane <86059679+Flaminghurricane@users.noreply.github.com>
2022-03-29 14:51:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByjCBgl9aR1RJ1cyls2xUcfP4vGMcSeQ0",
+  authDomain: "storytellingapp-6ccfe.firebaseapp.com",
+  databaseURL: "https://storytellingapp-6ccfe-default-rtdb.firebaseio.com",
+  projectId: "storytellingapp-6ccfe",
+  storageBucket: "storytellingapp-6ccfe.appspot.com",
+  messagingSenderId: "481422152800",
+  appId: "1:481422152800:web:3471ac8124f08c11d51f05"
+};
